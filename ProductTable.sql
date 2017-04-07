558ddf495230305b318d01324474c32ad631fd7b
serrabellum <serrabellum@gmail.com>
2017-04-07 13:22:30 -0500

    Update ProductTable.sql

--- ProductTable.sql
+++ ProductTable.sql
@@ -1,10 +1,16 @@
-CREATE TYPE active AS ENUM ('yes', 'no');
-CREATE TABLE ProductTable (
-	productID integer PRIMARY KEY NOT NULL,
-	lookupCode integer,
-	quantity integer,
-	createdOn date,
-	productKey integer,
-	price numeric,
-	active active
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE product (
+  id uuid NOT NULL,
+  lookupcode character varying(32) NOT NULL DEFAULT(''),
+  count int NOT NULL DEFAULT(0),
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT product_pkey PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
 );
+
+CREATE INDEX ix_product_lookupcode
+  ON product
+  USING btree
+  (lower(lookupcode::text) COLLATE pg_catalog."default");
