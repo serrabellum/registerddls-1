97eb691f2902fcb7398fe0b9e2d8a9a36dfeecb1
serrabellum <serrabellum@gmail.com>
2017-04-11 15:08:50 -0500

    Update ProductTable.sql

--- ProductTable.sql
+++ ProductTable.sql
@@ -1,12 +1,12 @@
 CREATE EXTENSION "uuid-ossp";
 
 CREATE TABLE product (
-  id uuid NOT NULL,
+  id uuid SERIAL NOT NULL,
   lookupcode character varying(32) NOT NULL DEFAULT(''),
   count int NOT NULL DEFAULT(0),
   price numeric NOT NULL,
   createdon timestamp without time zone NOT NULL DEFAULT now(),
-  CONSTRAINT product_pkey PRIMARY KEY (id)
+  CONSTRAINT productid PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
 );
