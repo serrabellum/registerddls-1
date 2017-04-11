36474136d6ca6cd56b95d8cedb7272da6887f51d
serrabellum <serrabellum@gmail.com>
2017-04-11 14:49:37 -0500

    Update ProductTable.sql

--- ProductTable.sql
+++ ProductTable.sql
@@ -4,6 +4,7 @@ CREATE TABLE product (
   id uuid NOT NULL,
   lookupcode character varying(32) NOT NULL DEFAULT(''),
   count int NOT NULL DEFAULT(0),
+  price numeric NOT NULL,
   createdon timestamp without time zone NOT NULL DEFAULT now(),
   CONSTRAINT product_pkey PRIMARY KEY (id)
 ) WITH (
