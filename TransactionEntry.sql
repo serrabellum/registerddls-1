b934d257adba581e2d6b312995cb87955e8e9655
serrabellum <serrabellum@gmail.com>
2017-04-19 18:14:15 -0500

    Update TransactionEntry.sql

--- TransactionEntry.sql
+++ TransactionEntry.sql
@@ -1,7 +1,16 @@
-CREATE TABLE TransactionEntry (
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE transactionEntry (
+	id uuid SERIAL NOT NULL,
 	quantity integer NOT NULL,
 	productName text,
-	productid integer REFERENCES Product productid,
+	productid integer REFERENCES product productid,
 	price numeric,
-	transactionid integer REFERENCES Transaction transactionid 
+	transactionid integer REFERENCES transaction transactionid 
+) WITH (
+  OIDS=FALSE
 );
+
+CREATE INDEX ix_transaction_transactionid
+  ON transaction
+  USING hash(transactionid);
