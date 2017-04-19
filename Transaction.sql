545f7a32032d0de031291f40a3deea857fc5c92c
serrabellum <serrabellum@gmail.com>
2017-04-19 18:08:15 -0500

    Update Transaction.sql

--- Transaction.sql
+++ Transaction.sql
@@ -1,9 +1,16 @@
-CREATE TABLE Transaction (
+CREATE TABLE transaction (
 	id uuid NOT NULL,
 	transactionid integer NOT NULL,
 	employeeid integer REFERENCES employee (employeeid),
-	totalCost numeric,
-	transactionType character varying(32),
-	date date,
+	totalCost numeric NOT NULL,
+	transactionType character varying(32) NOT NULL,
+	date date NOT NULL,
 	products json
+	CONSTRAINT transactionid PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
 );
+
+CREATE INDEX ix_transaction_transactionid
+  ON transaction
+  USING hash(transactionid);
