0779d2f286e52d07130388c33b5edc1fa8dc10fe
serrabellum <serrabellum@gmail.com>
2017-04-19 21:18:22 -0500

    Update TransactionEntry.sql

--- TransactionEntry.sql
+++ TransactionEntry.sql
@@ -1,12 +1,12 @@
 CREATE EXTENSION "uuid-ossp";
 
 CREATE TABLE transactionEntry (
-	id uuid SERIAL NOT NULL,
+	id uuid NOT NULL,
 	quantity integer NOT NULL,
 	productName text,
-	productid integer REFERENCES product productid,
+	productid character varying REFERENCES product productid,
 	price numeric,
-	transactionid integer REFERENCES transaction transactionid 
+	transactionid character varying REFERENCES transaction transactionid 
 ) WITH (
   OIDS=FALSE
 );
