bacfce63b91bd06979ac9309f2c588428baf47f3
serrabellum <serrabellum@gmail.com>
2017-04-19 21:53:39 -0500

    Update ProductTable.sql

--- ProductTable.sql
+++ ProductTable.sql
@@ -1,17 +1,9 @@
-CREATE EXTENSION "uuid-ossp";
-
 CREATE TABLE product (
-  id uuid SERIAL NOT NULL,
-  lookupcode character varying(32) NOT NULL DEFAULT(''),
-  count int NOT NULL DEFAULT(0),
-  price numeric NOT NULL,
-  createdon timestamp without time zone NOT NULL DEFAULT now(),
-  CONSTRAINT productid PRIMARY KEY (id)
+   id uuid NOT NULL,
+   lookupcode character varying(32) NOT NULL DEFAULT(''),
+   count int NOT NULL DEFAULT(0),
+   createdon timestamp without time zone NOT NULL DEFAULT now(),
+   CONSTRAINT product_pkey PRIMARY KEY (id)
 ) WITH (
-  OIDS=FALSE
+   OIDS=FALSE
 );
-
-CREATE INDEX ix_product_lookupcode
-  ON product
-  USING btree
-  (lower(lookupcode::text) COLLATE pg_catalog."default");
